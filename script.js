d603ea38ab1e3b77f2974763c2f2f486d8a05169
Shyam kumar <149792700+Shyamkumarsingh@users.noreply.github.com>
2024-10-07 00:03:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = '/downlodePdf/img/ElectricityBill_prog.pdf'; // Change this to the actual path of your PDF
+    link.href = '/MarsalRock/ElectricityBill_prog.pdf'; // this is path of the folder
     link.download = 'ElectricityBill_prog.pdf'; // The name of the file to be downloaded
     link.click();
   });
 
   document.getElementById('downloadBtn2').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = '/downlodePdf/notes.pdf'; // Change this to the actual path of your PDF
+    link.href = '/MarsalRock/notes.pdf'; // this to the actual path of PDF
     link.download = 'notes.pdf'; // The name of the file to be downloaded
     link.click();
   });
